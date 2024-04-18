e65723f5eb728c2f3313902f3da511ed14d93d8e
Sanya Stepko <thisissony666@gmail.com>
2024-04-18 10:16:44 +0300

    Added basic module for tg_bot

---
+++ handlers/__init__.py
@@ -0,0 +1,4 @@
+from .command_handlers import *
+from .message_handlers import *
+from .conversation_handler import *
+from .base_handler import BaseHandler
---
+++ handlers/command_handlers/__init__.py
@@ -0,0 +1,3 @@
+from .hello_handler import HelloHandler
+from .start_handler import StartHandler
+from .bye_handler import ByeHandler
